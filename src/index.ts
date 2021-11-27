0830aee9077472da4bd56cf88ce3ac25fb2eb089
Gina White <ginabythebay@gmail.com>
2021-11-27 15:22:02 -0800

    first version

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+var possibleEvents = new Set(["input", "onpropertychange", "keyup", "change", "paste"]);
+
+window.onload = () => {
+    var drillInput = document.getElementById("drill_diameter") as HTMLInputElement;
+    possibleEvents.forEach((eventName: string) => {
+        drillInput.addEventListener(eventName, (ev: Event) => {
+            var inputElement = ev.target as HTMLInputElement;
+            drill(inputElement)
+        })
+    });
+};
+
+function drill(inputElement: HTMLInputElement) {
+    const sfm = 250 // for Aluminum
+    const diameter = Number(inputElement.value);
+    const rpm = Math.round((3.8197 / diameter) * sfm)
+    const ipr = Math.min(.25, .001 * (diameter / .0625))
+    const ipm = ipr * rpm
+    console.log("ipr = " + ipr)
+
+    setLabel("sfm", sfm)
+    setLabel("rpm", rpm)
+    setLabel("ipm", ipm.toFixed(1))
+    setLabel("depth", (diameter * 4).toFixed(3))
+}
+
+function setLabel(id: string, value: number | string) {
+    var output = document.getElementById(id) as HTMLLabelElement;
+    output.innerHTML = String(value)
+}
---
+++ src/index.ts~
@@ -0,0 +1,12 @@
+var possibleEvents = new Set(["input", "onpropertychange", "keyup", "change", "paste"]);
+
+window.onload = () => {
+    var ticksInput = document.getElementById("ticks") as HTMLInputElement;
+    possibleEvents.forEach((eventName: string) => {
+        ticksInput.addEventListener(eventName, (ev: Event) => {
+            var inputElement = ev.target as HTMLInputElement;
+            var handler = new TickInputHandler();
+            handler.showResult(inputElement);
+        })
+    });
+};
