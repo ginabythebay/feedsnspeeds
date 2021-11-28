2792674e5d46bff6d4d77aab83d219be09c3dc5f
Gina White <ginabythebay@gmail.com>
2021-11-27 17:11:17 -0800

    allow material choices

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,47 @@
 var possibleEvents = new Set(["input", "onpropertychange", "keyup", "change", "paste"]);
 
 window.onload = () => {
-    var drillInput = document.getElementById("drill_diameter") as HTMLInputElement;
+    const materialsMenu = document.getElementById("material") as HTMLSelectElement;
+    const drillInput = document.getElementById("drill_diameter") as HTMLInputElement;
+    const calculator: Calculator = new Calculator(materialsMenu, drillInput)
     possibleEvents.forEach((eventName: string) => {
-        drillInput.addEventListener(eventName, (ev: Event) => {
-            var inputElement = ev.target as HTMLInputElement;
-            drill(inputElement)
+        drillInput.addEventListener(eventName, () => {
+            calculator.calc()
         })
     });
+    materialsMenu.onchange = () => {
+        calculator.calc();
+    }
     // calcs value on page reload if something was already entered
-    drill(drillInput)
+    calculator.calc();
 };
 
-function drill(inputElement: HTMLInputElement) {
-    const sfm = 250 // for Aluminum
-    const diameter = Number(inputElement.value);
-    const rpm = Math.round((3.8197 / diameter) * sfm)
-    const ipr = Math.min(.25, .001 * (diameter / .0625))
-    const ipm = ipr * rpm
-    console.log("ipr = " + ipr)
+class Calculator {
+    materialsMenu: HTMLOptionsCollection;
+    diameterElement: HTMLInputElement;
 
-    setLabel("sfm", sfm)
-    setLabel("rpm", rpm)
-    setLabel("ipm", ipm.toFixed(1))
-    setLabel("depth", (diameter * 4).toFixed(3))
+    constructor(materialsMenu: HTMLSelectElement, diameterElement: HTMLInputElement) {
+        this.materialsMenu = materialsMenu.options;
+        this.diameterElement = diameterElement;
+    }
+
+    calc() {
+        const material = this.materialsMenu.item(this.materialsMenu.selectedIndex) as HTMLOptionElement;
+        const val = material.value;
+        const sfm = Number(val);
+
+
+        const diameter = Number(this.diameterElement.value);
+        const rpm = Math.round((3.8197 / diameter) * sfm)
+        const ipr = Math.min(.25, .001 * (diameter / .0625))
+        const ipm = ipr * rpm
+        console.log("ipr = " + ipr)
+
+        setLabel("sfm", sfm)
+        setLabel("rpm", rpm)
+        setLabel("ipm", ipm.toFixed(1))
+        setLabel("depth", (diameter * 4).toFixed(3))
+    }
 }
 
 function setLabel(id: string, value: number | string) {
